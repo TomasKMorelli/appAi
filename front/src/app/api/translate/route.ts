a5921fd7f7b47db5b6697c6ac749f6dfe1a2466a
TomasKMorelli <totimorelli123@gmail.com>
2025-07-10 17:22:32 -0300

    c

--- front/src/app/api/translate/route.ts
+++ front/src/app/api/translate/route.ts
@@ -1,6 +1,6 @@
 import { NextRequest, NextResponse } from "next/server";
 import { translateText } from "@/helper/Openai";
-
+export const runtime = 'edge';
 
 export async function POST(req: NextRequest) {
   const { text, language } = await req.json();
